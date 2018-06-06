6b9bcbf19dcd6c94c1c2cfcd20a3e2a62c4ab152
Marc Etcheverry <marc@taplightsoftware.com>
2018-06-06 12:02:34 -0700

    Initial Commit

---
+++ SimpleExtensions/SimpleExtensions.h
@@ -0,0 +1,19 @@
+//
+//  SimpleExtensions.h
+//  SimpleExtensions
+//
+//  Created by Marc Etcheverry on 6/6/18.
+//  Copyright © 2018 Tap Light Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SimpleExtensions.
+FOUNDATION_EXPORT double SimpleExtensionsVersionNumber;
+
+//! Project version string for SimpleExtensions.
+FOUNDATION_EXPORT const unsigned char SimpleExtensionsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SimpleExtensions/PublicHeader.h>
+
+
